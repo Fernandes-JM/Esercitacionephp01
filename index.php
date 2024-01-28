351d148aa8586a9289210d987aa44a67508117a6
manuelfv24 <jossef.v62@gmail.com>
2024-01-28 20:47:35 +0100

    esercizi-php

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+//fatto
+   
+    // $integer = 10;
+    // $float = 1.34;
+    // $string = "hola";
+    // $bolean = true;
+
+    // var_dump($integer);
+    // var_dump($float);
+    // var_dump($string);
+    // var_dump($bolean);
+
+    // date le sigienti variabili:
+    // non valido
+
+//     $1text = "Marco";
+//     $texto2 = "hai";
+//     $text.3 = "sete"
+//     $text4 = "perche"
+//     $te-xt6  = '$text2'
+//     @text5  = '$beduto'
+//     $text 7 = 'beduto'
+//     $text8  = "tutto" 
+
+    // $text1 = "Marco";
+    // $text2 = "hai";
+    // $text3 = "sete";
+    // $text_4 = "?";
+    // $text_5  = "perche";
+    // $text_6  = '$text2';
+    // $text_7 = "bevuto";
+    // $text_8  = "tutto";
+    // $texto10 = "$text1 $text2 $text3 $text_5 $text2 $text_7 $text_8 ";
+
+    // var_dump($texto10);
+
+// Dati i seguienti arrey:
+
+$words1 = ['una', 'vita', '67', 'colle', 'mi','rosso', ['oscura', 'era', '89', ['mezzo', ['cammin', 'nel', ['selva', 'la', ['via', 'una', true,] , ] ], 'ritrovati', 'per', ]  ], 'diritta']; 
+
+print_r($words1);
+
+$resultado = "{$words1[6][3][1][1]}, {$words1[6][3][0]}, {di}, {$words1[6][3][1][0]},  {di}, {nostra}, {$words1[1]}, {$words1[4]}, {$words1[6][3][1][2][7]}, ";                       
+echo $resultado
+
+?>
