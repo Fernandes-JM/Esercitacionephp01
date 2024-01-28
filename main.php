ca65bf90d6ebb5be7dc8f57903eeac6e62387d38
manuelfv24 <jossef.v62@gmail.com>
2024-01-28 15:42:39 +0100

    esercizi.php01

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+// $empleados = [
+//     ['name' => 'Manuel', 'surname' => 'Fernandes' , 'gender' => 'm'],
+//     ['name' => 'Carlos', 'surname' => 'Hernandes' , 'gender' => 'nb'],
+//     ['name' => 'Carmen', 'surname' => 'Gonzalez' , 'gender' => 'f'],
+// ];
+//     foreach ($empleados as $empleado){
+     
+//     if($empleado['gender'] == 'm') 
+//         echo "Ciao buongiorno Sr. {$empleado['name']} {$empleado['surname']}\n";
+//     else if ($empleado['gender'] = 'f')
+//         echo "Ciao buongiorno Sr. {$empleado['name']} {$empleado['surname']}\n"; 
+//     else echo "Ciao buongiorno {$empleado['name']} {$empleado['surname']}\n";   
+
+
+// var_dump($empleados);
+// }     
+
+// // traccia 2:
+
+// $numeros = ['24', '26', '30', '45','51','632','743', '86', '99', '110', '113', '122', '131', '1478', '155', '146', '178', '1845', '19', '2320'];
+ 
+
+
+
